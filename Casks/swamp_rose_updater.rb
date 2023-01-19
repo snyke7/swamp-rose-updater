e1c7a400ef782d980bc23e3c6b4c6737788ffeb1
Ike Mulder <ikemulder@hotmail.com>
2023-01-19 10:35:35 +0100

    Added cask for use with homebrew

---
+++ Casks/swamp_rose_updater.rb
@@ -0,0 +1,11 @@
+cask "swamp_rose_updater" do
+  version "0.1"
+  sha256 ""
+
+  url "https://github.com/snyke7/swamp-rose-updater/releases/download/continuous/swamp_rose_updater.dmg"
+  name "swamp_rose_updater"
+  desc "Update your ROSE Online installation on MacOS"
+  homepage "https://github.com/snyke7/swamp-rose-updater"
+
+  app "swamp_rose_updater.app"
+end
